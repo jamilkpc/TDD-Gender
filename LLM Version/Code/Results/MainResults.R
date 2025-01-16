09c04db2f6b36d5aafe38fe3f8f3f2f2e3276cce
Jamil Civitarese <jamilkpc@gmail.com>
2025-01-16 09:25:36 -0500

    Updating Leisure Metrics

--- LLM Version/Code/Results/MainResults.R
+++ LLM Version/Code/Results/MainResults.R
@@ -30,3 +30,10 @@ summary(model)
 rdpower(data = cbind(Y,R), tau = sd(Y[abs(R)<model$bws[1]], na.rm = T)*0.2, covs = X, cluster = DictCounts$id_municipio, alpha = 0.05)
 rdpower(data = cbind(Y,R), tau = sd(Y[abs(R)<model$bws[1]], na.rm = T)*0.2, covs = X, cluster = DictCounts$id_municipio, alpha = 0.1)
 
+R <- DictCounts$vote_margin
+Y <- DictCounts$leisure_count
+
+model <- rdrobust(Y, R, 0, covs = X, cluster = DictCounts$id_municipio)
+summary(model)
+rdpower(data = cbind(Y,R), tau = sd(Y[abs(R)<model$bws[1]], na.rm = T)*0.2, covs = X, cluster = DictCounts$id_municipio, alpha = 0.05)
+rdpower(data = cbind(Y,R), tau = sd(Y[abs(R)<model$bws[1]], na.rm = T)*0.2, covs = X, cluster = DictCounts$id_municipio, alpha = 0.1)
